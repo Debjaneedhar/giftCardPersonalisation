beece227c18f3fc0562d0086207126e175be3f33
ddhar <debjanee_dhar@gap.com>
2023-08-21 17:53:52 +0530

    Repo for hackathon 2023

---
+++ src/main/java/com/hackathon/giftCardPersonalisation/GiftCardPersonalisationApplication.java
@@ -0,0 +1,12 @@
+package com.hackathon.giftCardPersonalisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftCardPersonalisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftCardPersonalisationApplication.class, args);
+	}
+}
